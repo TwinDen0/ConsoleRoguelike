6583f0675ed0aaba8b7b7c328386d01267125659
Денис Коваленко <103291152+TwinDen0@users.noreply.github.com>
2024-10-16 23:11:09 +1000

    main display

--- Roguelike/Roguelike/Program.cs
+++ Roguelike/Roguelike/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Roguelike {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Кодировка на UTF-8
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Menu menu = new Menu();
+        menu.Run();
     }
 }
